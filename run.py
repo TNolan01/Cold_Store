cc06e203b029e80ba6e9aa4657b9d9563b26aeea
TNolan01 <ihavesevenfingers@gmail.com>
2022-02-22 10:43:21 +0000

    intital commit with creation of class for room dimensions

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Dimensions:
+    def __init__(self, measurement, dist):
+        self.measurement = measurement
+        self.dist = dist
+
+    def validator(self):
+        while True:
+            try:
+              value = float(input())
+            except ValueError:
+                print("Sorry, please enter a numerical value.\n")
+                continue
+            if value < 0:
+                print("Sorry, you must enter a positive numerical value.\n")
+                continue
+            else:
+                return value
+    
+    def room(self):
+        m1 = Dimensions("Length", 0.0)
+        m1.dist = Dimensions.validator("Please enter the length of the coldroom in metres :\n " )
+        m2 = Dimensions("Width", 0.0)
+        m2.dist = Dimensions.validator("Please enter the width of the coldroom in metres :\n ")
+        m3 = Dimensions("height", 0.0)
+        m3.dist = Dimensions.validator("Please enter the height of the coldroom in metres :\n ")
+    
+        
+ 
+Dimensions.room("Please enter details for coldroom")
+room = Dimensions()
+print (room)
+
