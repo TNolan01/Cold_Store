10516bfe1fcbbebcefdc6d141b67b8940d95881b
TNolan01 <ihavesevenfingers@gmail.com>
2022-02-25 10:30:45 +0000

    correct yes no loop in Dimensions class

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ class Dimensions:
     def __init__(self, measurement, dist):
         self.measurement = measurement
         self.dist = dist
-
+           
+    
+    
     def validator(self):
         while True:
             try:
@@ -22,19 +24,17 @@ class Dimensions:
         cold room. This will calculate volume and surface area
         also.
         """
-        m1 = Dimensions("Length", 0.0)
         print("Please enter the length of the coldroom in metres : ")
         m1.dist = Dimensions.validator(self)
-        print("The", m1.measurement, "you entered is", m1.dist, "metres.")
-        m2 = Dimensions("Width", 0.0)
         print("Please enter the Width of the coldroom in metres : ")
         m2.dist = Dimensions.validator(self)
-        print("The", m2.measurement, "you entered is", m2.dist, "metres.")
-        m3 = Dimensions("height", 0.0)
         print("Please enter the height of the coldroom in metres : ")
         m3.dist = Dimensions.validator(self)
+        print("The", m1.measurement, "you entered is", m1.dist, "metres.")
+        print("The", m2.measurement, "you entered is", m2.dist, "metres.")
         print("The", m3.measurement, "you entered is", m3.dist, "metres.")
-        Dimensions.yes_no("Are entered values correct, please enter yes or no ? \n")
+        Dimensions.yes_no("Are entered values correct, please enter yes or no ? \n") 
+        return
         
 
     def yes_no(prompt):
@@ -43,8 +43,8 @@ class Dimensions:
         response from user.
     
         """
-        yes = {'y', 'ye', 'yes', ''}
-        no = {'n', 'no'}
+        yes = {'yes'}
+        no = {'no'}
         while True:
             try:
                 value = input(prompt)
@@ -52,15 +52,23 @@ class Dimensions:
                 print("That aint right")
                 continue
             if value in yes:
-                return
+                print("Calculating volume.....")
+                m4.dist = Dimensions.volume(m1.dist, m2.dist, m3.dist)
+                print("Calculated volume......", m4.dist, "metres cubed.")
+                break
             elif value in no:
-                print("Please input correct data.")
-                Dimensions.room('')
-            else:
+                Dimensions.room("Please input correct measurements.")
+                break
+            else: 
                 print("Please respond with a yes or no answer.")
     
 
-    def volume():
+    def volume(length, height, width):
+        """
+        Function to calculate volume of the room
+        """
+        m4.dist = length * height * width
+        return m4.dist
         
 
 
@@ -71,5 +79,10 @@ class Dimensions:
 """
 Start of program.
 """
- 
+m1 = Dimensions("Length", 0.0)
+m2 = Dimensions("Width", 0.0)
+m3 = Dimensions("Height", 0.0)
+m4 = Dimensions("Volume", 0.0)
 Dimensions.room('')
+
+
